99cbf529c4cac13ac89debe9d81b11a44edefe9b
BejoJeffrin23 <bejojeffrin23@gmail.com>
2019-10-16 07:23:33 +0530

    alarm-check2

--- app/libs/socketLib.js
+++ app/libs/socketLib.js
@@ -108,7 +108,7 @@ let setServer = (server) => {
                             for (let x of result){
                                 m = (new Date(x.start).getMonth())
                                 d = (new Date(x.start).getDay())                        
-                                if (min== x.startMinute && hours == x.startHour && month == m && day == d) {
+                                if (min== x.startMinute && hours+5 == x.startHour+5 && month == m && day == d) {
                                 console.log(x.userId)
                                 controller.sendAlarmMail(x.userId, x.title, x.adminName)
                                 data = { adminName: x.adminName, userId: x.userId, title: x.title }
